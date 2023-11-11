11c16922769da820fe7fefc5ce08438bb3b519f0
siuslawcarrie <102423932+siuslawcarrie@users.noreply.github.com>
2023-11-10 17:53:51 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,18 +1,32 @@
+/*Factors of a Positive Integer
+ * Carrie Bailey
+ * Due: 10/22/2023
+ * Completed: 11/10/2023
+ * Version 1.0
+ * Description:
+ * • Use a loop to ask the user to input a positive integer. If the integer is not positive, tell the user it isn’t valid.
+Continue until you get a positive integer. Note: 0 is not positive.
+• Use a 2nd type of loop which calculates the factors of the integer and display them.
+• Your solution should use 2 different loop types. Use comments in your solution to explain why you chose
+each loop type.
+ */
 #include <iostream>
 
 using namespace std;
 
 int validation_integer(int min,
-                       int max);
+                       int max);//function declaration to validate positive integer
 
-int get_number();
+int get_number();//function declaration to ask user to input a positive integer
 
 int main() {
-    int n = get_number();
-
-    cout << "Factors of " << n << " are"; //for loop here because it's efficient - there is a clear starting point, it increments easily, and there is a
-    // limiter, all in one line of code. A while loop could be used, but would be more lines of code and would potentially be more confusing.
-    for (int i = 1; i <= n; ++i) {
+    //VARIABLES
+    int n = get_number();//assign variable n to positive integer
+    //END VARIABLES
+    //DISPLAY LINE 27 & 31
+    cout << "Factors of " << n << " are";
+    for (int i = 1; i <= n; ++i) {//for loop here because it's efficient - there is a clear starting point, it increments easily, and there is a
+        // limiter, all in one line of code. A while loop could be used, but would be more lines of code and would potentially be more confusing.
         if (n % i == 0) {
             cout << " " << i;
         }
@@ -21,8 +35,8 @@ int main() {
 
     return 0;
 }
-
-int get_number() {
+//FUNCTION DEFINITIONS
+int get_number() {//get user number
     cout << "Enter a positive integer: ";
     int min = 1;
     int max = 99999999;
@@ -51,6 +65,7 @@ int validation_integer(int min,
         cin >> number;
         cout << endl;
     }
+    //END FUNCTIONS
     return number;
 }
 
